6ee8a8f8130752b535e142cbfae33421c74d5e6c
Tobias Frantsen <tobias-frantsen@hotmail.com>
2017-01-02 12:50:47 +0100

    hej

--- app/src/main/java/com/example/magnus/laesomondo/MainMenu.java
+++ app/src/main/java/com/example/magnus/laesomondo/MainMenu.java
@@ -6,12 +6,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main_menu);
     }
 
