8b9555d45f5892b936db8fbf8d7769bacf978ae5
Rasmus Edvardsen <rasmus.edvardsen@hotmail.com>
2017-01-16 14:38:52 +0100

    timer fungerer

--- app/src/main/java/com/example/magnus/laesomondo/fragments/MainMenu.java
+++ app/src/main/java/com/example/magnus/laesomondo/fragments/MainMenu.java
@@ -2,12 +2,14 @@ package com.example.magnus.laesomondo.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.magnus.laesomondo.R;
+import com.example.magnus.laesomondo.dataclasses.Timer;
 
 public class MainMenu extends Fragment {
 
@@ -25,6 +27,9 @@ public class MainMenu extends Fragment {
         textFromNetButton = (Button)v.findViewById(R.id.mainMenuWebcontentButton);
         laeseTestButton  = (Button)v.findViewById(R.id.mainMenuUserLoggedInReadingTestButton);
 
+        final Timer timer = new Timer();
+        timer.start();
+
         View.OnClickListener listener = new View.OnClickListener() {
 
             @Override
@@ -32,7 +37,7 @@ public class MainMenu extends Fragment {
                 int buttonId = view.getId();
                 switch (buttonId){
                     case R.id.mainMenuLogInButton:
-
+                        Log.i("time", String.valueOf(timer.getTimeToPrint()));
                         getFragmentManager().beginTransaction()
                                 .setCustomAnimations(R.animator.enter_from_left, R.animator.exit_to_left,
                                         R.animator.exit_to_right, R.animator.enter_from_right )
