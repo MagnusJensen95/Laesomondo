b4fcbdae90e4918768d73e5beec3660f52830b01
Tobias <tobias-frantsen@hotmail.com>
2017-01-17 17:44:20 +0100

    Fix små bugs med textfromnet

--- app/src/main/java/com/example/magnus/laesomondo/fragments/TextFromNetAct.java
+++ app/src/main/java/com/example/magnus/laesomondo/fragments/TextFromNetAct.java
@@ -5,11 +5,13 @@ import android.app.Fragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +22,9 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.magnus.laesomondo.R;
+import com.nhaarman.supertooltips.ToolTip;
+import com.nhaarman.supertooltips.ToolTipRelativeLayout;
+import com.nhaarman.supertooltips.ToolTipView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -28,17 +33,61 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
+import static java.lang.Thread.sleep;
+
 public class TextFromNetAct extends Fragment {
 
     WebView web;
     FloatingActionButton load;
+    ToolTipView myToolTipView;
+    private Thread t;
+    ToolTipRelativeLayout toolTipRelativeLayout;
+    View v;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.fragment_text_from_net, container, false);
+        v = inflater.inflate(R.layout.fragment_text_from_net, container, false);
         firstRunPreferences();
+
+     toolTipRelativeLayout = (ToolTipRelativeLayout) v.findViewById(R.id.activity_main_tooltipRelativeLayout);
+
+        final ToolTip toolTip = new ToolTip()
+                .withText("Hent tekst")
+                .withTextColor(Color.WHITE)
+                .withColor(getResources().getColor(R.color.colorPrimaryDark))
+                .withShadow()
+                .withAnimationType(ToolTip.AnimationType.FROM_TOP);
+        myToolTipView = toolTipRelativeLayout.showToolTipForView(toolTip,v.findViewById(R.id.floatingActionButton));
+
+
+     t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+
+                    synchronized(this){
+                        wait(3000);
+                        if(!(getActivity() == null)) {
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    myToolTipView.performClick();
+                                }
+                            });
+                        }
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
+
+
 
         web = (WebView) v.findViewById(R.id.webviewContent);
 
@@ -66,7 +115,7 @@ public class TextFromNetAct extends Fragment {
             setRunned();
             new AlertDialog.Builder(getActivity(), R.style.MyDialogTheme)
                     .setTitle("Valg af tekst")
-                    .setMessage("Gå ind på en hjemmeside,og find en tekst eller artikel og klik på kanppen Hent")
+                    .setMessage("Gå ind på en hjemmeside,og find en tekst eller artikel og klik på ikonet hent")
                     .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
                         }
@@ -78,6 +127,7 @@ public class TextFromNetAct extends Fragment {
         }
 
         return v;
+
     }
 
 
